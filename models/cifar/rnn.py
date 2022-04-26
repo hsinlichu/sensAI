a7e8debc1db9771e6e0f3c626381cdfdd372b46d
WilliamLiuAtCPC <47164078+WilliamLiuAtCPC@users.noreply.github.com>
2022-04-26 12:29:14 -0700

    Update rnn.py

--- models/cifar/rnn.py
+++ models/cifar/rnn.py
@@ -1,30 +1,31 @@
-import torch
 from torch import nn
 
-
 class lstm(nn.Module):
-    def __init__(self, hidden_size=128, num_layers=1, num_classes=1000):
+    def __init__(self,h_size=64,img_width=32*3, num_classes = 10):
         super(lstm, self).__init__()
 
-        self.rnn = nn.LSTM(          # if use nn.RNN(), it hardly learns
-            input_size=96,
-            hidden_size=hidden_size, # rnn hidden unit
-            num_layers=num_layers,   # number of rnn layer
-            batch_first=True,        # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        self.rnn = nn.LSTM(         # if use nn.RNN(), it hardly learns // LSTM
+            input_size=img_width,
+            hidden_size=h_size,         # rnn hidden unit
+            num_layers=3,           # number of rnn layer
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
         )
 
-        self.out = nn.Linear(hidden_size, num_classes)
+        self.out = nn.Linear(h_size, num_classes)
 
     def forward(self, x, features_only=False):
+        # flatten
+        x = x.permute(0, 2, 3, 1)
+        x = x.contiguous().view(-1, 32, 32 * 3)
         # x shape (batch, time_step, input_size)
         # r_out shape (batch, time_step, output_size)
         # h_n shape (n_layers, batch, hidden_size)
         # h_c shape (n_layers, batch, hidden_size)
-        r_out, (h_n, h_c) = self.rnn(x, None)   # None represents zero initial hidden state
-
+        r_out, (h_n, h_c) = self.rnn(x, None)   # None represents zero initial hidden state # for lstm
+        # r_out, h_n = self.rnn(x, None) # for rnn
         # choose r_out at the last time step
-        x = r_out[:, -1, :]
-        if features_only:
-            return x
-        out = self.out(x)
-        return out
+        if not features_only:
+            output = self.out(r_out[:, -1, :])
+            return output
+
+        return r_out[:, -1, :]
